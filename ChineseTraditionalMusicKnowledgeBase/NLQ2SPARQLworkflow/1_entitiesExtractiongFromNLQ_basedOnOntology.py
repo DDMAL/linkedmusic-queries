08b9b37808eea219393615fee0263686c7170c01
candlecao <529360500@qq.com>
2025-03-20 10:12:04 -0400

    Update 1_entitiesExtractiongFromNLQ_basedOnOntology.py

--- ChineseTraditionalMusicKnowledgeBase/NLQ2SPARQLworkflow/1_entitiesExtractiongFromNLQ_basedOnOntology.py
+++ ChineseTraditionalMusicKnowledgeBase/NLQ2SPARQLworkflow/1_entitiesExtractiongFromNLQ_basedOnOntology.py
@@ -38,7 +38,7 @@ with open("ontologySnippet_objectProperties_simplified.ttl", "r") as context3:
 with open("ontologySnippet_dataProperties_simplified.ttl", "r") as context4:
     context_ontology_dataProperty = context4.readlines()
 # The natural language question is read from a text file:
-with open("sampleQuestions/question_MusicType_MusicType1.txt", 'r') as f:
+with open("sampleQuestions/question_PieceWithPerformance_MusicType.txt", 'r') as f:
     question = f.readlines()
 
 prompt0 = f"""
